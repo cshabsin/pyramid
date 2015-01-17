057a8ebfcc74a0ff5f96c65d5e3bb5c98ba1dc3b
Jonathan Wang <jtwang@dhcp-18-111-50-27.dyn.mit.edu>
2015-01-17 12:11:44 -0500

    reformat properties.out

--- gatherProperties.pl
+++ gatherProperties.pl
@@ -1,22 +1,26 @@
 while (<>)
   {
-	 if (/This is a word with property (\w+)/) {
-		$mode = 0;
-		$property = $1;
+	 if (/^===/) {
+		%propertyMap = ();
+	 } elsif (/This is a word with property (\w+)/) {
+		$propertyMap{$1} = 0;
 	 } elsif (/This is NOT a word with property (\w+)/) {
-		$mode = 1;
-		$property = $1;
+		$propertyMap{$1} = 1;
 	 } elsif (/\[/) {
 		chomp;
 		s/[\[\]' ]//g;
 		my @words = split /,/;
 		next if $#words > 0;
-		push (@{$properties{$property}->[$mode]}, $words[0]);
+		while (my ($p, $type) = each %propertyMap) {
+		  push (@{$properties{$p}->[$type]}, $words[0]);
+		}
 	 }
   }
 
 while (my ($property, $v) = each %properties)
   {
-	 print "$property: " . join(',', @{$v->[0]}) . "\n";
-	 print "NOT $property: " . join(',', @{$v->[1]}) . "\n";
+	 print "===\n$property\n===\n";
+	 print join("\n", map(uc, @{$v->[0]})) . "\n";
+	 print "===\nNOT $property\n===\n";
+	 print join("\n", map(uc, @{$v->[1]})) . "\n";
   }
