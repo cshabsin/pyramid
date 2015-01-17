b6ed647c9262b9975a8d7fb9539d83598fcb813f
Jonathan Wang <jtwang@dhcp-18-111-50-27.dyn.mit.edu>
2015-01-16 23:59:15 -0500

    SHA1 BASE-26

--- util.py
+++ util.py
@@ -1,2 +1,10 @@
+import re
+import string
+
 def sum_of_letters_a1(word):
     return sum([ord(c.upper())-64 for c in word])
+
+def base26(word):
+    word = word.upper()
+    table = string.maketrans("ABCDEFGHIJKLMNOPQRSTUVWXYZ","0123456789ABCDEFGHIJKLMNOP")
+    return int(word.translate(table), 26)
