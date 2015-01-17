9cb4be48dc60f851f78d691ff41f01d72139db7f
Chris Shabsin <cshabsin@gmail.com>
2015-01-17 15:49:18 -0500

    Update previous with current values. (8331/10000 found) Make pyramid_csv smarter.

--- pyramid_csv.py
+++ pyramid_csv.py
@@ -6,10 +6,33 @@ import handlers
 import anagram
 import caesar
 
+def GetSafe(arr, row, col):
+    try:
+        return arr[row][col]
+    except:
+        return ""
+
+def PutSafe(arr, row, col, val):
+    while row >= len(arr):
+        arr.append([])
+    while col >= len(arr[row]):
+        arr[row].append("")
+    arr[row][col] = val
+
+    
 previous = []
 with open("previous.csv", "r") as prevfile:
     for line in prevfile:
         previous.append(line.strip().split(","))
+#with open("previous2.csv", "r") as prevfile:
+#    rownum = 0
+#    for line in prevfile:
+#        colnum = 0
+#        for entry in line.strip().split(","):
+#            if not GetSafe(previous, rownum, colnum) and entry:
+#                PutSafe(previous, rownum, colnum, entry)
+#            colnum += 1
+#        rownum += 1
 
 with open("words.txt", "r") as wordfile:
     all_words = [line.strip() for line in wordfile]
@@ -26,22 +49,27 @@ output = open("output.csv", "w")
             
 def emit(output_str):
     output.write(output_str)
-            
+
+total = 0
+found = 0
 for rownum in xrange(125):
   for colnum in xrange(142):
-    sys.stderr.write("(%d, %d)     \r" % (rownum, colnum))
-    if previous[rownum][colnum]:
-        emit(previous[rownum][colnum] + ",")
-        continue
     filename = "row%d/row%d_col%d.txt" % (rownum, rownum, colnum)
     if not os.path.exists(filename):
         emit(",")
+        continue
+    total += 1
+    sys.stderr.write("(%d, %d)     \r" % (rownum, colnum))
+    if GetSafe(previous, rownum, colnum):
+        emit(GetSafe(previous, rownum, colnum) + ",")
+        found += 1
         continue
     words = all_words[:]
     with open(filename, "r") as f:
         rules = [line.strip() for line in f]
         for (priority, handler_list) in sorted(handlers.ALL_HANDLERS.items()):
             for h in handler_list:
+                if not words: break
                 delInx = []
                 for inx in xrange(len(rules)):
                     rule = rules[inx]
@@ -53,8 +81,11 @@ for rownum in xrange(125):
 
         if len(words) == 1:
             emit(words[0].upper() + ",")
+            found += 1
         else:
             if not words:
                 sys.stderr.write("No words found at (%d, %d)\n" % (rownum, colnum))
             emit(",")
   emit("\n")
+
+print "Found %d of %d entries." % (found, total)
