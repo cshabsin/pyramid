a13e9b31a876d1b255346fc341b353a4e8a83383
Chris Shabsin <cshabsin@gmail.com>
2015-01-16 22:41:11 -0500

    Add SumLetterHandler

--- cshabsin.py
+++ cshabsin.py
@@ -1,4 +1,5 @@
 import re
+import util
 
 LENGTHRANGE_RE = re.compile(
     r"Length: between (\d*) and (\d*) \(inclusive\) letters") 
@@ -33,11 +34,28 @@ class StartVowelHandler(object):
     @staticmethod
     def prune(line, words):
         val = STARTVOWEL_RE.match(line).group(1) == "YES"
-        print val
         def starts_with_vowel(word):
             return word[0].upper() in "AEIOU"
         words = [word for word in words if starts_with_vowel(word) == val]
         return words
+
+
+SUMLETTER_RE = re.compile(
+    r"Sum of letters \(A=1, B=2, etc\) is divisible by (\d*): (.*)")
+class SumLetterHandler(object):
+    @staticmethod
+    def matches(line):
+        return SUMLETTER_RE.match(line)
+
+    @staticmethod
+    def prune(line, words):
+        m = SUMLETTER_RE.match(line)
+        modulo = int(m.group(1))
+        is_divisible = m.group(2) == "YES"
+        words = [word for word in words
+                 if (util.sum_of_letters_a1(word)%modulo == 0) == is_divisible]
+        return words
+
     
 
-ALL_HANDLERS = [LengthHandler, StartVowelHandler]
+ALL_HANDLERS = [LengthHandler, StartVowelHandler, SumLetterHandler]
