7f0ae1180dd297fde4b7966981cd9990adb9ef99
Chris Shabsin <cshabsin@gmail.com>
2015-01-16 23:09:48 -0500

    Print out unmatched lines.

--- pyramid.py
+++ pyramid.py
@@ -8,9 +8,13 @@ filename = sys.argv[1]
 f = open(filename, "r")
 for line in f:
     line = line.strip()
+    any_matched = False
     for handler in handlers.ALL_HANDLERS:
         if handler.matches(line):
+            any_matched = True
             words = handler.prune(line, words)
+    if not any_matched:
+        print line
 
 if len(words) > 20000:
     print "Too many: %d" % len(words)
